c8e3babf4381dd195c5cf46f365ab3d0aee16791
Shahi Hari <harishahi@QAIT.COM>
2019-07-31 10:17:18 +0530

    Practice hai

---
+++ src/main/java/qainfotech/Esehi/App.java
@@ -0,0 +1,18 @@
+package qainfotech.Esehi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        String s  = "A.B.B.C.D.V.R.E.S.S.";
+        String [] a = s.split(".");
+//        for(String s1 : a)
+//        	System.out.println(s1);
+        System.out.println(a);
+    }
+}
